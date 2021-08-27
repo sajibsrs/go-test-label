e21e9d2adc59591883b92c462b9ace959bdd6cb0
Sajidur Rahman <sajibsrs@gmail.com>
2021-08-27 23:30:40 +0600

    added tests labels

---
+++ label_test.go
@@ -0,0 +1,28 @@
+package label
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLabel(t *testing.T) {
+	label := newlabel(false)
+
+	t.Logf("Next test should pass")
+	t.Logf("%sTest that passed", label.pass)
+
+	fmt.Println()
+
+	t.Logf("Next test should fail")
+	t.Logf("%sTest that failed", label.fail)
+
+	fmt.Println()
+
+	t.Logf("Next test should have info")
+	t.Logf("%sTest that displayed some info", label.info)
+
+	fmt.Println()
+
+	t.Logf("Next test should have warning")
+	t.Logf("%sTest that generated warning", label.warn)
+}
