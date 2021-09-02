0f8793cfde7184ee5e4e8e09e47791f390aee6c5
Sajidur Rahman <sajibsrs@gmail.com>
2021-09-02 19:58:33 +0600

    visibility change

--- label.go
+++ label.go
@@ -1,22 +1,22 @@
 package label
 
-type Label struct {
+type label struct {
 	Pass string
 	Fail string
 	Info string
 	Warn string
 }
 
-func NewLabel(block bool) *Label {
+func NewLabel(block bool) *label {
 	if block {
-		return &Label{
+		return &label{
 			Pass: "\033[42;30m Pass ✓ \033[0m ",
 			Fail: "\033[41;30m Fail ✕ \033[0m ",
 			Info: "\033[46;30m Info i \033[0m ",
 			Warn: "\033[43;30m Warn ! \033[0m ",
 		}
 	}
-	return &Label{
+	return &label{
 		Pass: "\033[32;1mPass ✓ \033[0m ",
 		Fail: "\033[31;1mfail ✕ \033[0m ",
 		Info: "\033[36;1minfo i \033[0m ",
